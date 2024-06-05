4bda31b9389c53580c08b377aeb168836c5574b2
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-06-05 14:34:40 +0200

    readapting ConnectionHandlerInterface to use setting struct

--- src/ConnectionHandlerInterface.h
+++ src/ConnectionHandlerInterface.h
@@ -29,6 +29,7 @@
 #include <Client.h>
 #include <Udp.h>
 #include "ConnectionHandlerDefinitions.h"
+#include "settings/settings.h"
 
 /******************************************************************************
    TYPEDEFS
@@ -77,6 +78,17 @@ class ConnectionHandler {
     void addDisconnectCallback(OnNetworkEventCallback callback) __attribute__((deprecated));
     void addErrorCallback(OnNetworkEventCallback callback) __attribute__((deprecated));
 
+    /**
+     * Update the interface settings. This can be performed only when the interface is
+     * in INIT state. otherwise nothing is performed. The type of the interface should match
+     * the type of the settings provided
+     */
+    inline void updateSetting(const models::NetworkSetting& s) {
+      if(_current_net_connection_state == NetworkConnectionState::INIT && s.type == _interface) {
+        memcpy(&_settings, &s, sizeof(s));
+      }
+    }
+
   protected:
 
     bool _keep_alive;
@@ -88,6 +100,11 @@ class ConnectionHandler {
     virtual NetworkConnectionState update_handleDisconnecting() = 0;
     virtual NetworkConnectionState update_handleDisconnected () = 0;
 
+<<<<<<< HEAD:src/ConnectionHandlerInterface.h
+=======
+    models::NetworkSetting _settings;
+
+>>>>>>> 8ee4dcd (readapting ConnectionHandlerInterface to use setting struct):src/Arduino_ConnectionHandlerInterface.h
   private:
 
     unsigned long _lastConnectionTickTime;
