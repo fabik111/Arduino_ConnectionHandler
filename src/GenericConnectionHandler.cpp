891d9667488453495449ecd335b873ed419a8f5d
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-12-05 10:33:51 +0100

    defining a wrapper connection handler with the objective of being generic

---
+++ src/GenericConnectionHandler.cpp
@@ -0,0 +1,147 @@
+/*
+   This file is part of ArduinoIoTCloud.
+   Copyright 2020 ARDUINO SA (http://www.arduino.cc/)
+   This software is released under the GNU General Public License version 3,
+   which covers the main part of arduino-cli.
+   The terms of this license can be found at:
+   https://www.gnu.org/licenses/gpl-3.0.en.html
+   You can be released from the requirements of the above licenses by purchasing
+   a commercial license. Buying such a license is mandatory if you want to modify or
+   otherwise use the software for commercial activities involving the Arduino
+   software without disclosing the source code of your own applications. To purchase
+   a commercial license, send an email to license@arduino.cc.
+*/
+
+/******************************************************************************
+   INCLUDE
+ ******************************************************************************/
+
+#include "GenericConnectionHandler.h"
+#include "Arduino_ConnectionHandler.h"
+
+static inline ConnectionHandler* instantiate_handler(NetworkAdapter adapter);
+
+bool GenericConnectionHandler::updateSetting(const models::NetworkSetting& s) {
+
+    if(_ch != nullptr && _ch->_current_net_connection_state != NetworkConnectionState::INIT) {
+        // If the internal connection handler is already being used and not in INIT phase we cannot update the settings
+        return false;
+    } else if(_ch != nullptr && _ch->_current_net_connection_state == NetworkConnectionState::INIT && _interface != s.type) {
+        // If the internal connection handler is already being used and in INIT phase and the interface type is being changed
+        // -> we need to deallocate the previously allocated handler
+
+        // if interface type is not being changed -> we just need to call updateSettings
+        delete _ch;
+        _ch = nullptr;
+    }
+
+    if(_ch == nullptr) {
+        _ch = instantiate_handler(s.type);
+    }
+
+    _interface = s.type;
+    return _ch != nullptr ? _ch->updateSetting(s) : false;
+}
+
+NetworkConnectionState GenericConnectionHandler::update_handleInit() {
+    return _ch != nullptr ? _ch->update_handleInit() : NetworkConnectionState::INIT;
+}
+
+NetworkConnectionState GenericConnectionHandler::update_handleConnecting() {
+    return _ch != nullptr ? _ch->update_handleConnecting() : NetworkConnectionState::INIT;
+}
+
+NetworkConnectionState GenericConnectionHandler::update_handleConnected() {
+    return _ch != nullptr ? _ch->update_handleConnected() : NetworkConnectionState::INIT;
+}
+
+NetworkConnectionState GenericConnectionHandler::update_handleDisconnecting() {
+    return _ch != nullptr ? _ch->update_handleDisconnecting() : NetworkConnectionState::INIT;
+}
+
+NetworkConnectionState GenericConnectionHandler::update_handleDisconnected() {
+    return _ch != nullptr ? _ch->update_handleDisconnected() : NetworkConnectionState::INIT;
+}
+
+#if not (defined(BOARD_HAS_LORA) or defined(BOARD_HAS_NOTECARD))
+unsigned long GenericConnectionHandler::getTime() {
+    return _ch != nullptr ? _ch->getTime() : 0;
+}
+
+Client & GenericConnectionHandler::getClient() { // FIXME _ch may be nullptr
+    return _ch->getClient();
+}
+
+UDP & GenericConnectionHandler::getUDP() { // FIXME _ch may be nullptr
+    return _ch->getUDP();
+}
+
+#endif // not (defined(BOARD_HAS_LORA) or defined(BOARD_HAS_NOTECARD))
+
+void GenericConnectionHandler::connect() {
+    if(_ch!=nullptr) {
+        _ch->connect();
+    }
+}
+
+void GenericConnectionHandler::disconnect() {
+    if(_ch!=nullptr) {
+        _ch->disconnect();
+    }
+}
+
+void GenericConnectionHandler::addCallback(NetworkConnectionEvent const event, OnNetworkEventCallback callback) {
+    if(_ch!=nullptr) {
+        _ch->addCallback(event, callback);
+    }
+}
+
+static inline ConnectionHandler* instantiate_handler(NetworkAdapter adapter) {
+    switch(adapter) {
+        #if defined(BOARD_HAS_WIFI)
+        case NetworkAdapter::WIFI:
+            return new WiFiConnectionHandler();
+            break;
+        #endif
+
+        #if defined(BOARD_HAS_ETHERNET)
+        case NetworkAdapter::ETHERNET:
+            return new EthernetConnectionHandler();
+            break;
+        #endif
+
+        #if defined(BOARD_HAS_NB)
+        case NetworkAdapter::NB:
+            return new NBConnectionHandler();
+            break;
+        #endif
+
+        #if defined(BOARD_HAS_GSM)
+        case NetworkAdapter::GSM:
+            return new GSMConnectionHandler();
+            break;
+        #endif
+
+        #if defined(BOARD_HAS_CATM1_NBIOT)
+        case NetworkAdapter::CATM1:
+            return new CatM1ConnectionHandler();
+            break;
+        #endif
+
+        #if defined(BOARD_HAS_CELLULAR)
+        case NetworkAdapter::CELL:
+            return new CellularConnectionHandler();
+            break;
+        #endif
+
+        // #if defined(BOARD_HAS_NOTECARD) // FIXME understand how to adapt it to the settings structure
+        // case NOTECARD:
+        //     return new NotecardConnectionHandler();
+        //     break;
+        // #endif
+
+        default:
+            Debug.print(DBG_ERROR, "Network adapter not supported by this platform: %d", adapter);
+            return nullptr;
+    }
+}
---
+++ src/GenericConnectionHandler.h
@@ -0,0 +1,70 @@
+/*
+   This file is part of ArduinoIoTCloud.
+
+   Copyright 2019 ARDUINO SA (http://www.arduino.cc/)
+
+   This software is released under the GNU General Public License version 3,
+   which covers the main part of arduino-cli.
+   The terms of this license can be found at:
+   https://www.gnu.org/licenses/gpl-3.0.en.html
+
+   You can be released from the requirements of the above licenses by purchasing
+   a commercial license. Buying such a license is mandatory if you want to modify or
+   otherwise use the software for commercial activities involving the Arduino
+   software without disclosing the source code of your own applications. To purchase
+   a commercial license, send an email to license@arduino.cc.
+*/
+
+#ifndef ARDUINO_GENERIC_CONNECTION_HANDLER_H_
+#define ARDUINO_GENERIC_CONNECTION_HANDLER_H_
+
+/******************************************************************************
+   INCLUDE
+ ******************************************************************************/
+
+#include "ConnectionHandlerInterface.h"
+
+/******************************************************************************
+   CLASS DECLARATION
+ ******************************************************************************/
+
+/** GenericConnectionHandler class
+ * This class aims to wrap a connectionHandler and provide a generic way to
+ * instantiate a specific connectionHandler type
+ */
+class GenericConnectionHandler : public ConnectionHandler
+{
+  public:
+
+    GenericConnectionHandler(bool const keep_alive=false): ConnectionHandler(keep_alive), _ch(nullptr) {}
+
+    #if defined(BOARD_HAS_NOTECARD) || defined(BOARD_HAS_LORA)
+      virtual bool available() = 0;
+      virtual int read() = 0;
+      virtual int write(const uint8_t *buf, size_t size) = 0;
+    #else
+      unsigned long getTime() override;
+      Client & getClient() override;
+      UDP & getUDP() override;
+    #endif
+
+    bool updateSetting(const models::NetworkSetting& s) override;
+
+    void connect() override;
+    void disconnect() override;
+    void addCallback(NetworkConnectionEvent const event, OnNetworkEventCallback callback) override;
+
+  protected:
+
+    NetworkConnectionState update_handleInit         () override;
+    NetworkConnectionState update_handleConnecting   () override;
+    NetworkConnectionState update_handleConnected    () override;
+    NetworkConnectionState update_handleDisconnecting() override;
+    NetworkConnectionState update_handleDisconnected () override;
+
+  private:
+
+    ConnectionHandler* _ch;
+};
+
+#endif /* ARDUINO_GENERIC_CONNECTION_HANDLER_H_ */
